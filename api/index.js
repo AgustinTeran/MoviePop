eb54735f66e1ff99f1fd0737dd512c9405ef027c
agustinteran <agustintern@gmail.com>
2022-11-24 11:18:32 -0300

    fixing deployment error

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
+require("pg")
 var server = require("./src/server")
 var {sequelize} = require("./src/db")
 
 
-server.listen(3001,() => {
-    sequelize.sync({force: false})
+server.listen(3001,async() => {
+    await sequelize.sync({force: false})
     console.log("server listening on port 3001");
 })
